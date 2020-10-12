bc403cf14f45468a7eb297cd216f5691a27f6c7f
yihuang <yi.codeplayer@gmail.com>
2020-10-12 12:02:27 +0800

    Problem (fix #164): the docker image built by CI is not pushed into github container registry automatically
    Solution:
    - build image and push to github container registry when pushed to
      master or tag created
    - pass environment to the embeded supervisord process
    - set version flags through ldflags

--- pystarport/default.nix
+++ pystarport/default.nix
@@ -3,6 +3,14 @@ let chain = import ../. { inherit pkgs; };
 in
   pkgs.poetry2nix.mkPoetryApplication {
     projectDir = ./.;
+    # FIXME remove after merged: https://github.com/nix-community/poetry2nix/pull/189
+    overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
+      supervisor = super.supervisor.overridePythonAttrs (
+        old: {
+          propagatedBuildInputs = with pkgs.python3Packages; [ meld3 setuptools ];
+        }
+      );
+    });
     preBuild = ''
     sed -i -e 's@CHAIN = "chain-maind"  # edit by nix-build@CHAIN = "${chain}/bin/chain-maind"@' pystarport/cluster.py
     '';
