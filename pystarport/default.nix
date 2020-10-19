dec1f8a9dd3a744d0b0113932d536364148e8920
yihuang <huang@crypto.com>
2020-10-19 09:02:11 +0800

    Problem (fix #173): Missing ledger tags when build in nix (#184)
    Solution:
    - enable `runVend = true` to fix build with c dependencies
    - add tags to build with ledger
    - add ledger_zemu flag to build zemu and not zemu versions
      use zemu version in integration tests shell, real
      version in docker image

--- pystarport/default.nix
+++ pystarport/default.nix
@@ -1,17 +1,17 @@
-{ pkgs ? import <nixpkgs> {} }:
-let chain = import ../. { inherit pkgs; };
-in
-  pkgs.poetry2nix.mkPoetryApplication {
-    projectDir = ./.;
-    # FIXME remove after merged: https://github.com/nix-community/poetry2nix/pull/189
-    overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
-      supervisor = super.supervisor.overridePythonAttrs (
-        old: {
-          propagatedBuildInputs = with pkgs.python3Packages; [ meld3 setuptools ];
-        }
-      );
-    });
-    preBuild = ''
-    sed -i -e 's@CHAIN = "chain-maind"  # edit by nix-build@CHAIN = "${chain}/bin/chain-maind"@' pystarport/cluster.py
-    '';
-  }
+{ pkgs ? import <nixpkgs> {},
+  chaind ? import ../. { inherit pkgs; }
+}:
+pkgs.poetry2nix.mkPoetryApplication {
+  projectDir = ./.;
+  # FIXME remove after merged: https://github.com/nix-community/poetry2nix/pull/189
+  overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
+    supervisor = super.supervisor.overridePythonAttrs (
+      old: {
+        propagatedBuildInputs = with pkgs.python3Packages; [ meld3 setuptools ];
+      }
+    );
+  });
+  preBuild = ''
+  sed -i -e 's@CHAIN = "chain-maind"  # edit by nix-build@CHAIN = "${chaind}/bin/chain-maind"@' pystarport/cluster.py
+  '';
+}
