39bf1f863370c2b745664ddb659e28072b661a83
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2020-12-15 17:26:52 +0800

    updated sources

--- pystarport/pystarport/convert.sh
+++ pystarport/pystarport/convert.sh
@@ -5,8 +5,8 @@ TENDERMINT=./tendermint
 TMP=$(whereis grpc_python_plugin)
 PLUGIN="$(cut -d' ' -f2 <<<"$TMP")"
 mkdir $OUTPUT
-git clone --branch v0.40.0-rc2 https://github.com/cosmos/cosmos-sdk.git
-git clone --branch v0.34.0-rc5 https://github.com/tendermint/tendermint.git
+git clone --branch v0.40.0-rc5 https://github.com/cosmos/cosmos-sdk.git
+git clone --branch v0.34.0 https://github.com/tendermint/tendermint.git
 cp -Rf $COSMOS/third_party/proto/* $COSMOS/proto/ 
 # cosmos
 protoc --proto_path=$COSMOS/proto --proto_path=$COSMOS/third_party/proto --python_out=$OUTPUT $(find $COSMOS/proto/cosmos -iname "*.proto") --grpc_python_out=$OUTPUT  --plugin=protoc-gen-grpc_python=$PLUGIN
