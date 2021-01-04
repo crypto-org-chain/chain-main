3230d83f969db7eadef6fbe24f0683c3986463a0
Devashish Dixit <devashishdxt@gmail.com>
2021-01-04 10:39:56 +0800

    Problem: Empty genesis validation function
    Solution: Added a comment explaining the reason for not performing any validations. Fixes #333.

--- x/supply/types/genesis.go
+++ x/supply/types/genesis.go
@@ -8,5 +8,6 @@ func DefaultGenesis() *GenesisState {
 // Validate performs basic genesis state validation returning an error upon any
 // failure.
 func (gs GenesisState) Validate() error {
+	// Since the genesis state for supply module is empty, we don't have any validations to perform.
 	return nil
 }
