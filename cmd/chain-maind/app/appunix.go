c8c9340282735db92763d1561ed33a248588ebd9
Jongwhan Lee <51560997+leejw51crypto@users.noreply.github.com>
2021-03-16 18:19:26 +0900

    Problem: chain-main can't be built for windows (fix #406) (#409)
    Solution: use ioutil.WriteFile for Windows

---
+++ cmd/chain-maind/app/appunix.go
@@ -0,0 +1,13 @@
+// +build linux darwin
+
+package app
+
+import (
+	"os"
+
+	"github.com/google/renameio"
+)
+
+func WriteFile(filename string, data []byte, perm os.FileMode) error {
+	return renameio.WriteFile(filename, data, perm)
+}
