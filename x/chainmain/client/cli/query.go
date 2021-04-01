bf3ce0d52c169d0780cf050e3bbb86dab07b30a1
linfeng-crypto <56661925+linfeng-crypto@users.noreply.github.com>
2021-04-01 15:16:17 +0800

    Problem: no aggregated TX history command(fixes #285) (#433)
    Solution: add a new query command txs-all to query all the events transactions

--- x/chainmain/client/cli/query.go
+++ x/chainmain/client/cli/query.go
@@ -10,7 +10,9 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/rest"
 	"github.com/cosmos/cosmos-sdk/version"
+	authclient "github.com/cosmos/cosmos-sdk/x/auth/client"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 
 	"github.com/crypto-org-chain/chain-main/v1/config"
@@ -33,6 +35,52 @@ func GetQueryCmd() *cobra.Command {
 	chainmainQueryCmd.AddCommand(GetBalancesCmd())
 
 	return chainmainQueryCmd
+}
+
+func QueryAllTxCmd() *cobra.Command {
+	// returns a command to search through transactions by address.
+	cmd := &cobra.Command{
+		Use:   "txs-all [address]",
+		Short: "Query for all  paginated transactions by address",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, queryErr := client.GetClientQueryContext(cmd)
+			if queryErr != nil {
+				return queryErr
+			}
+			addrStr := args[0]
+			_, addressErr := sdk.AccAddressFromBech32(addrStr)
+			if addressErr != nil {
+				return addressErr
+			}
+
+			var tmEvents []string
+			events := [...]string{"message.sender", "transfer.recipient", "withdraw_rewards.validator", "ibc_transfer.receiver", "ibc_transfer.sender"}
+			for _, event := range events {
+				eventPair := fmt.Sprintf("%s='%s'", event, addrStr)
+				tmEvents = append(tmEvents, eventPair)
+			}
+
+			page, pageErr := cmd.Flags().GetInt(flags.FlagPage)
+			if pageErr != nil {
+				return pageErr
+			}
+			limit, limitErr := cmd.Flags().GetInt(flags.FlagLimit)
+			if limitErr != nil {
+				return limitErr
+			}
+			txs, err := authclient.QueryTxsByEvents(clientCtx, tmEvents, page, limit, "")
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(txs)
+		},
+	}
+	cmd.Flags().StringP(flags.FlagNode, "n", "tcp://localhost:26657", "Node to connect to")
+	cmd.Flags().String(flags.FlagKeyringBackend, flags.DefaultKeyringBackend, "Select keyring's backend (os|file|kwallet|pass|test)")
+	cmd.Flags().Int(flags.FlagPage, rest.DefaultPage, "Query a specific page of paginated results")
+	cmd.Flags().Int(flags.FlagLimit, rest.DefaultLimit, "Query number of transactions results per page returned")
+	return cmd
 }
 
 func GetBalancesCmd() *cobra.Command {
