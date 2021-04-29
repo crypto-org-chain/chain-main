f828f433f2c80213a04436c6dc999e6e3809e5fd
Devashish Dixit <devashishdxt@gmail.com>
2021-04-29 17:22:19 +0800

    Add todo to remove duplicate code

--- x/nft/keeper/msg_server.go
+++ x/nft/keeper/msg_server.go
@@ -51,6 +51,7 @@ func (m msgServer) IssueDenom(goCtx context.Context, msg *types.MsgIssueDenom) (
 	return &types.MsgIssueDenomResponse{}, nil
 }
 
+// TODO: remove duplicate code in `MintNFT` and `EditNFT`
 // nolint: dupl
 func (m msgServer) MintNFT(goCtx context.Context, msg *types.MsgMintNFT) (*types.MsgMintNFTResponse, error) {
 	recipient, err := sdk.AccAddressFromBech32(msg.Recipient)
