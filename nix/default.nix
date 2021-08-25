942c97939e7bdbc5d97b347622eaf96094638cf2
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2021-08-25 12:03:23 +0800

    Problem: latest IBC relayer release is not tested
    Solution: bumped the hermes dependency to 0.7
    in nix integration tests

--- nix/default.nix
+++ nix/default.nix
@@ -15,7 +15,7 @@ import sources.nixpkgs {
       hermes = pkgs.rustPlatform.buildRustPackage rec {
         name = "hermes";
         src = sources.ibc-rs;
-        cargoSha256 = sha256:18xqp3fdvwxxz4yj343dg13ghhx0bls07nkrp0277q57s47sw2jx;
+        cargoSha256 = sha256:1sc4m4bshnjv021ic82c3m36pakf15xr5cw0dsrcjzs8pv3nq9cd;
         cargoBuildFlags = "-p ibc-relayer-cli";
         buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
           pkgs.darwin.apple_sdk.frameworks.Security
