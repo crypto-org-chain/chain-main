c8e44de33de0a9cf91644274d3225191a4efd8c1
Devashish Dixit <devashishdxt@gmail.com>
2022-03-22 23:45:42 +0800

    Problem: `.golanci.yml` contains a lot of unncessary linters (#734)
    Solution: Removed linters that are enabled by default and replaced deprecated linters with recommended ones + format code and fix lint errors
    
    * Upgrade golangci-lint version in ci

--- x/nft/types/keys.go
+++ x/nft/types/keys.go
@@ -66,7 +66,7 @@ func SplitKeyDenom(key []byte) (denomID, tokenID string, err error) {
 
 // KeyOwner gets the key of a collection owned by an account address
 func KeyOwner(address sdk.AccAddress, denomID, tokenID string) []byte {
-	key := append(PrefixOwners, delimiter...)
+	key := append(PrefixOwners, delimiter...) // nolint:gocritic
 	if address != nil {
 		key = append(key, []byte(address.String())...)
 		key = append(key, delimiter...)
@@ -85,7 +85,7 @@ func KeyOwner(address sdk.AccAddress, denomID, tokenID string) []byte {
 
 // KeyNFT gets the key of nft stored by an denom and id
 func KeyNFT(denomID, tokenID string) []byte {
-	key := append(PrefixNFT, delimiter...)
+	key := append(PrefixNFT, delimiter...) // nolint:gocritic
 	if len(denomID) > 0 {
 		key = append(key, []byte(denomID)...)
 		key = append(key, delimiter...)
@@ -99,18 +99,18 @@ func KeyNFT(denomID, tokenID string) []byte {
 
 // KeyCollection gets the storeKey by the collection
 func KeyCollection(denomID string) []byte {
-	key := append(PrefixCollection, delimiter...)
+	key := append(PrefixCollection, delimiter...) // nolint:gocritic
 	return append(key, []byte(denomID)...)
 }
 
 // KeyDenomID gets the storeKey by the denom id
 func KeyDenomID(id string) []byte {
-	key := append(PrefixDenom, delimiter...)
+	key := append(PrefixDenom, delimiter...) // nolint:gocritic
 	return append(key, []byte(id)...)
 }
 
 // KeyDenomName gets the storeKey by the denom name
 func KeyDenomName(name string) []byte {
-	key := append(PrefixDenomName, delimiter...)
+	key := append(PrefixDenomName, delimiter...) // nolint:gocritic
 	return append(key, []byte(name)...)
 }
