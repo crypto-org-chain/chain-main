346460093a4ffec2d5ca4eeba9f9ec55f7df82fb
Devashish Dixit <devashishdxt@gmail.com>
2022-05-16 13:35:38 +0800

    fix lint errors

--- app/app.go
+++ app/app.go
@@ -405,14 +405,20 @@ func New(
 
 	icaAuthModule := icaauthmodule.NewAppModule(appCodec, app.ICAAuthKeeper)
 
+	// TODO: Remove this `nolint` when fee middleware is added
+	// nolint: gosimple
 	var icaAuthStack porttypes.IBCModule
 	icaAuthStack = icaauthmodule.NewIBCModule(app.ICAAuthKeeper)
 	// icaAuthStack = ibcfee.NewIBCMiddleware(icaAuthStack, app.IBCFeeKeeper)
 
+	// TODO: Remove this `nolint` when fee middleware is added
+	// nolint: gosimple
 	var icaControllerStack porttypes.IBCModule
 	icaControllerStack = icacontroller.NewIBCMiddleware(icaAuthStack, app.ICAControllerKeeper)
 	// icaControllerStack = ibcfee.NewIBCMiddleware(icaControllerStack, app.IBCFeeKeeper)
 
+	// TODO: Remove this `nolint` when fee middleware is added
+	// nolint: gosimple
 	var icaHostStack porttypes.IBCModule
 	icaHostStack = icahost.NewIBCModule(app.ICAHostKeeper)
 	// icaHostStack = ibcfee.NewIBCMiddleware(icaHostStack, app.IBCFeeKeeper)
