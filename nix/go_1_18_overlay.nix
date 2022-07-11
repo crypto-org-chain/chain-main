91b851c1124cb6dbfeb3533e3f5f25e121e33690
HuangYi <huang@crypto.com>
2022-07-11 14:46:52 +0800

    fix build on linux

--- nix/go_1_18_overlay.nix
+++ nix/go_1_18_overlay.nix
@@ -1,9 +1,10 @@
 final: prev: {
-  go_1_18 = prev.go_1_18.override {
+  go_1_18 = prev.go_1_18.override ({
     inherit (final.darwin.apple_sdk_11_0.frameworks) Security Foundation;
-    inherit (final.darwin.apple_sdk_11_0) stdenv;
     xcbuild = prev.xcbuild.override {
       inherit (final.darwin.apple_sdk_11_0) stdenv;
     };
-  };
+  } // final.lib.optionalAttrs final.stdenv.isDarwin {
+    inherit (final.darwin.apple_sdk_11_0) stdenv;
+  });
 }
