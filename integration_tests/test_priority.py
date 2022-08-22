c18ce5881d3acc1b6a82a91a428c2f8690f42235
HuangYi <huang@crypto.com>
2022-08-22 14:32:07 +0800

    add priority integration test

---
+++ integration_tests/test_priority.py
@@ -0,0 +1,63 @@
+from .cosmoscli import ClusterCLI
+from .utils import wait_for_new_blocks
+
+PRIORITY_REDUCTION = 1000000
+
+
+def test_priority(cluster: ClusterCLI):
+    cli = cluster.cosmos_cli()
+    test_cases = [
+        {
+            "from": cli.address("community"),
+            "to": cli.address("validator"),
+            "amount": "1000aphoton",
+            "gas_prices": "1000basecro",
+        },
+        {
+            "from": cli.address("signer1"),
+            "to": cli.address("signer2"),
+            "amount": "1000aphoton",
+            "gas_prices": "2000basecro",
+        },
+        {
+            "from": cli.address("signer2"),
+            "to": cli.address("signer1"),
+            "amount": "1000aphoton",
+            "gas_prices": "3000basecro",
+        },
+        {
+            "from": cli.address("validator"),
+            "to": cli.address("community"),
+            "amount": "1000aphoton",
+            "gas_prices": "4000basecro",
+        },
+    ]
+    txs = []
+    for tc in test_cases:
+        tx = cli.transfer(
+            tc["from"],
+            tc["to"],
+            tc["amount"],
+            gas_prices=tc["gas_prices"],
+            generate_only=True,
+        )
+        txs.append(
+            cli.sign_tx_json(
+                tx, tc["from"], max_priority_price=tc.get("max_priority_price")
+            )
+        )
+
+    txhashes = []
+    for tx in txs:
+        rsp = cli.broadcast_tx_json(tx, broadcast_mode="sync")
+        assert rsp["code"] == 0, rsp["raw_log"]
+        txhashes.append(rsp["txhash"])
+
+    print("wait for two new blocks, so the sent txs are all included")
+    wait_for_new_blocks(cli, 2)
+
+    tx_results = [cli.tx_search_rpc(f"tx.hash='{txhash}'")[0] for txhash in txhashes]
+    tx_indexes = [(int(r["height"]), r["index"]) for r in tx_results]
+    print(tx_indexes)
+    # the first sent tx are included later, because of reversed priority order
+    assert all(i1 > i2 for i1, i2 in zip(tx_indexes, tx_indexes[1:]))
