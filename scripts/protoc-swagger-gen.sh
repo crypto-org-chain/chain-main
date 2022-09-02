b64e3606ee1baa63760f92b46909702b4d8df536
Devashish Dixit <devashishdxt@gmail.com>
2022-09-02 10:18:06 +0800

    Problem: Protobuf code generation is not consistent with cosmos-sdk
    Solution: Generate proto code using buf with similar configuration as cosmos-sdk

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -2,24 +2,20 @@
 
 set -eo pipefail
 
-mkdir -p ./tmp-swagger-gen
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-cosmos_proto_dirs=$(find ./third_party/cosmos-sdk/proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+# mkdir -p ./tmp-swagger-gen
+cd proto
+proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+cosmos_proto_dirs=$(find ../third_party/cosmos-sdk/proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs $cosmos_proto_dirs; do
-
   # generate swagger files (filter query files)
   query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
   if [[ ! -z "$query_file" ]]; then
-    buf protoc  \
-      -I "proto" \
-      -I "third_party/cosmos-sdk/proto" \
-      -I "third_party/cosmos-sdk/third_party/proto" \
-      "$query_file" \
-      --swagger_out=./tmp-swagger-gen \
-      --swagger_opt=logtostderr=true --swagger_opt=fqn_for_swagger_name=true --swagger_opt=simple_operation_ids=true
+    echo $query_file
+    buf generate --template buf.gen.swagger.yaml $query_file
   fi
 done
 
+cd ..
 # combine swagger files
 # uses nodejs package `swagger-combine`.
 # all the individual swagger files need to be configured in `config.json` for merging
