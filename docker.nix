9491fe2a6c9bef2d29e38d8d8dd6a22d5800c563
mmsqe <mavis@crypto.com>
2022-09-27 16:59:34 +0800

    fix make docker image

--- docker.nix
+++ docker.nix
@@ -1,17 +1,18 @@
 { system ? "x86_64-linux", pkgs ? import ./nix { inherit system; } }:
 let
   self = import ./. { inherit pkgs; };
+  chain-maind = pkgs.callPackage ./. {};
 in
 {
   chaindImage =
     pkgs.dockerTools.buildLayeredImage {
       name = "crypto-org-chain/chain-maind";
-      config.Entrypoint = [ "${self.chain-maind}/bin/chain-maind" ];
+      config.Entrypoint = [ "${chain-maind}/bin/chain-maind" ];
     };
 
   pystarportImage =
     pkgs.dockerTools.buildLayeredImage {
       name = "crypto-org-chain/chain-main-pystarport";
-      config.Entrypoint = [ "${self.pystarport}/bin/pystarport" ];
+      config.Entrypoint = [ "${pkgs.test-env}/bin/pystarport" ];
     };
 }
