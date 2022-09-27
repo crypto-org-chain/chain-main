9014b0fc6b0f8604488f1e158de06db011e3c693
mmsqe <mavis@crypto.com>
2022-09-27 17:43:46 +0800

    fix lint

--- docker.nix
+++ docker.nix
@@ -1,13 +1,12 @@
 { system ? "x86_64-linux", pkgs ? import ./nix { inherit system; } }:
 let
   self = import ./. { inherit pkgs; };
-  chain-maind = pkgs.callPackage ./. {};
 in
 {
   chaindImage =
     pkgs.dockerTools.buildLayeredImage {
       name = "crypto-org-chain/chain-maind";
-      config.Entrypoint = [ "${chain-maind}/bin/chain-maind" ];
+      config.Entrypoint = [ "${pkgs.callPackage ./. {}}/bin/chain-maind" ];
     };
 
   pystarportImage =
