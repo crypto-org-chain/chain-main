837fea1b5925217f41bdab02ecb405c571dc7b0c
Devashish Dixit <devashishdxt@gmail.com>
2022-10-20 12:36:19 +0530

    Update testenv.nix

--- nix/testenv.nix
+++ nix/testenv.nix
@@ -1,25 +1,29 @@
-{ poetry2nix, python39Packages, python39 }:
+{ poetry2nix, python39Packages, python39, lib }:
 poetry2nix.mkPoetryEnv {
   python = python39;
   projectDir = ../integration_tests;
-  overrides = poetry2nix.overrides.withDefaults (self: super: {
-    pyparsing = super.pyparsing.overridePythonAttrs (
-      old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.flit-core ];
-      }
-    );
-
-    platformdirs = python39Packages.platformdirs;
-
-    hdwallets = super.hdwallets.overridePythonAttrs (
-      old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.poetry ];
-      }
-    );
-    pystarport = super.pystarport.overridePythonAttrs (
-      old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.poetry ];
-      }
-    );
-  });
+  overrides = poetry2nix.overrides.withDefaults (lib.composeManyExtensions [
+    (self: super:
+      let
+        buildSystems = {
+          pyparsing = [ "flit-core" ];
+          hdwallets = [ "poetry" ];
+          pystarport = [ "poetry" ];
+          durations = [ "setuptools" ];
+          multitail2 = [ "setuptools" ];
+          pytest-github-actions-annotate-failures = [ "setuptools" ];
+          flake8-black = [ "setuptools" ];
+        };
+      in
+      lib.mapAttrs
+        (attr: systems: super.${attr}.overridePythonAttrs
+          (old: {
+            nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: self.${a}) systems;
+          }))
+        buildSystems
+    )
+    (self: super: {
+      platformdirs = python39Packages.platformdirs;
+    })
+  ]);
 }
