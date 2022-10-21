d3701292072b7d0e53e7da99da775890362e3fa6
Devashish Dixit <devashishdxt@gmail.com>
2022-10-21 10:57:52 +0530

    Add googleapis to python protobuf generation

--- pystarport/new-convert.sh
+++ pystarport/new-convert.sh
@@ -7,5 +7,6 @@ buf generate ../third_party/cosmos-sdk/proto
 buf generate buf.build/cosmos/cosmos-proto
 buf generate buf.build/cosmos/gogo-proto
 buf generate buf.build/tendermint/tendermint
+buf generate buf.build/googleapis/googleapis
 
 rm -rf ./tendermint
