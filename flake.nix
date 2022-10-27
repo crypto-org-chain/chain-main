a9700ebf0acf2478b2c8719c5283df28b8bba18b
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2022-10-27 16:00:25 +0800

    bump to go 1.19

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
           devShells = {
             chain-maind = pkgs.mkShell {
               buildInputs = with pkgs; [
-                go_1_18
+                go_1_19
                 rocksdb
               ];
             };
