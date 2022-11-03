b6c6b104ae0cfd67cf07d2974f307699924f09ba
mmsqe <mavis@crypto.com>
2022-11-03 11:14:59 +0800

    Problem: latest hermes is not used in integration tests (#894)
    Solution:
    * update hermes v1.1.0

--- nix/hermes.nix
+++ nix/hermes.nix
@@ -9,7 +9,7 @@
 rustPlatform.buildRustPackage rec {
   name = "hermes";
   inherit src;
-  cargoSha256 = "sha256-c4zEQKMg3xVct/iyj6MYH/MiACMiHVRBRgEuaLGH1Vg=";
+  cargoSha256 = "sha256-DBHDJlD2kTUeU0LCG5ZUij+v+lWUYRPGVvUKUYPfYQA=";
   cargoBuildFlags = "-p ibc-relayer-cli";
   buildInputs = lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.Security
