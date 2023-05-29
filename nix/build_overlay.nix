cd4b0b5efcf64c555c8d4c805396a7ad68901b0d
HuangYi <huang@crypto.com>
2023-05-29 11:33:36 +0800

    Problem: go 1.20.4 is not used

--- nix/build_overlay.nix
+++ nix/build_overlay.nix
@@ -6,10 +6,10 @@ final: super: {
   };
   rocksdb = final.callPackage ./rocksdb.nix { };
   go_1_20 = super.go_1_20.overrideAttrs (prev: rec {
-    version = "1.20.3";
+    version = "1.20.4";
     src = final.fetchurl {
       url = "https://go.dev/dl/go${version}.src.tar.gz";
-      hash = "sha256-5Ee0mM3lAhXE92GeUSSw/E4l+10W6kcnHEfyeOeqdjo=";
+      hash = "sha256-nzSs4Sh2S3o6SyOLgFhWzBshhDBN+eVpCCWwcQ9CAtY=";
     };
   });
 }
