0a39b373c547156a25e32c63bb4a7e4d41dedd62
mmsqe <mavis@crypto.com>
2024-01-16 17:14:08 +0800

    cleanup ica test

--- integration_tests/test_ica.py
+++ integration_tests/test_ica.py
@@ -1,12 +1,11 @@
 import json
 import subprocess
-import time
 from pathlib import Path
 
 import pytest
 
 from .ibc_utils import search_target, wait_relayer_ready
-from .utils import cluster_fixture
+from .utils import cluster_fixture, wait_for_fn
 
 pytestmark = pytest.mark.ibc
 
@@ -19,6 +18,57 @@ def cluster(worker_index, pytestconfig, tmp_path_factory):
         worker_index,
         tmp_path_factory.mktemp("data"),
     )
+
+
+def wait_for_check_channel_ready(cli, connid, channel_id, target="STATE_OPEN"):
+    print("wait for channel ready", channel_id, target)
+
+    def check_channel_ready():
+        channels = cli.ibc_query_channels(connid)["channels"]
+        try:
+            state = next(
+                channel["state"]
+                for channel in channels
+                if channel["channel_id"] == channel_id
+            )
+        except StopIteration:
+            return False
+        return state == target
+
+    wait_for_fn("channel ready", check_channel_ready)
+
+
+def assert_channel_open_init(rsp):
+    assert rsp["code"] == 0, rsp["raw_log"]
+    port_id, channel_id = next(
+        (
+            evt["attributes"][0]["value"],
+            evt["attributes"][1]["value"],
+        )
+        for evt in rsp["events"]
+        if evt["type"] == "channel_open_init"
+    )
+    print("port-id", port_id, "channel-id", channel_id)
+    return port_id, channel_id
+
+
+def wait_for_check_tx(cli, adr, num_txs, timeout=None):
+    print("wait for tx arrive")
+
+    def check_tx():
+        current = len(cli.query_all_txs(adr)["txs"])
+        print("current", current)
+        return current > num_txs
+
+    if timeout is None:
+        wait_for_fn("transfer tx", check_tx)
+    else:
+        try:
+            print(f"should assert timeout err when pass {timeout}s")
+            wait_for_fn("transfer tx", check_tx, timeout=timeout)
+        except TimeoutError:
+            raised = True
+        assert raised
 
 
 def start_and_wait_relayer(cluster):
@@ -56,42 +106,21 @@ def test_ica(cluster, tmp_path):
     addr_host = cluster["ica-host-1"].address("signer")
 
     # create interchain account
-    rsp = json.loads(
-        cli_controller.raw(
-            "tx",
-            "icaauth",
-            "register-account",
-            controller_connection,
-            "-y",
-            from_=addr_controller,
-            home=cli_controller.data_dir,
-            node=cli_controller.node_rpc,
-            keyring_backend="test",
-            chain_id=cli_controller.chain_id,
-        )
+    rsp = cli_controller.icaauth_register_account(
+        controller_connection,
+        from_=addr_controller,
+        gas="400000",
     )
-    if rsp["code"] == 0:
-        rsp = cli_controller.event_query_tx_for(rsp["txhash"])
 
     assert rsp["code"] == 0, rsp["raw_log"]
-
-    # FIXME more stable way to wait for relaying
-    time.sleep(20)
+    _, channel_id = assert_channel_open_init(rsp)
+    wait_for_check_channel_ready(cli_controller, controller_connection, channel_id)
 
     # get interchain account address
-    ica_address = json.loads(
-        cli_controller.raw(
-            "query",
-            "icaauth",
-            "interchain-account-address",
-            controller_connection,
-            addr_controller,
-            output="json",
-            node=cli_controller.node_rpc,
-            chain_id=cli_controller.chain_id,
-        )
+    ica_address = cli_controller.ica_query_account(
+        controller_connection,
+        addr_controller,
     )["interchainAccountAddress"]
-
     # initial balance of interchain account should be zero
     assert cli_host.balance(ica_address) == 0
 
@@ -112,32 +141,24 @@ def test_ica(cluster, tmp_path):
     with open(generated_tx, "w") as opened_file:
         json.dump(generated_tx_msg, opened_file)
 
+    no_timeout = 60
     num_txs = len(cli_host.query_all_txs(ica_address)["txs"])
 
-    # submit transaction on host chain on behalf of interchain account
-    rsp = json.loads(
-        cli_controller.raw(
-            "tx",
-            "icaauth",
-            "submit-tx",
+    def submit_msgs(timeout_in_s=no_timeout, gas="200000"):
+        # submit transaction on host chain on behalf of interchain account
+        rsp = cli_controller.icaauth_submit_tx(
             controller_connection,
             generated_tx,
-            "-y",
+            timeout_duration=f"{timeout_in_s}s",
+            gas=gas,
             from_=addr_controller,
-            home=cli_controller.data_dir,
-            node=cli_controller.node_rpc,
-            keyring_backend="test",
-            chain_id=cli_controller.chain_id,
         )
-    )
+        assert rsp["code"] == 0, rsp["raw_log"]
+        timeout = timeout_in_s + 3 if timeout_in_s < no_timeout else None
+        wait_for_check_tx(cli_host, ica_address, num_txs, timeout)
 
-    assert rsp["code"] == 0, rsp["raw_log"]
-
-    # FIXME more stable way to wait for relaying
-    time.sleep(20)
-
+    submit_msgs()
     # check if the transaction is submitted
     assert len(cli_host.query_all_txs(ica_address)["txs"]) == num_txs + 1
-
     # check if the funds are reduced in interchain account
     assert cli_host.balance(ica_address) == 50000000
