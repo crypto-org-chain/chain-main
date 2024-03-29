5d523778a7cf14b137d6259c157de4a4df67efe1
yihuang <huang@crypto.com>
2024-03-29 15:16:19 +0800

    Problem: slashing fix not applied on mainnet (#1040)
    * Problem: slashing fix not applied on mainnet
    
    Solution:
    - add mainnet softfork height
    
    * fix workflow

--- app/forks.go
+++ app/forks.go
@@ -7,6 +7,7 @@ import (
 var SlashingForkHeights = map[string]int64{
 	"testnet-croeseid-4":                    14857500,
 	"tempcrypto-org-chain-mainnet-dryrun-1": 6782000,
+	"crypto-org-chain-mainnet-1":            16978800,
 }
 
 func SlashingForkEnabled(ctx sdk.Context) bool {
