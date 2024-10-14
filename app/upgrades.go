452e0889206f07eb5aa153640f65557483853a41
mmsqe <mavis@crypto.com>
2024-10-14 09:23:29 +0800

    Problem: MsgModuleQuerySafe is not in allowed messages when upgrade (#1084)
    * Problem: MsgModuleQuerySafe is not in allowed messages when upgrade
    
    * fix
    
    * Update app/upgrades.go
    
    Signed-off-by: mmsqe <mavis@crypto.com>
    
    * Apply suggestions from code review
    
    Signed-off-by: mmsqe <mavis@crypto.com>
    
    ---------
    
    Signed-off-by: mmsqe <mavis@crypto.com>

--- app/upgrades.go
+++ app/upgrades.go
@@ -3,18 +3,34 @@ package app
 import (
 	"context"
 	"fmt"
+	"slices"
 
 	storetypes "cosmossdk.io/store/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	clientkeeper "github.com/cosmos/ibc-go/v8/modules/core/02-client/keeper"
 )
 
-func (app *ChainApp) RegisterUpgradeHandlers(cdc codec.BinaryCodec, clientKeeper clientkeeper.Keeper) {
+func (app *ChainApp) RegisterUpgradeHandlers(cdc codec.BinaryCodec) {
 	planName := "v5.0"
 	app.UpgradeKeeper.SetUpgradeHandler(planName, func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return app.ModuleManager.RunMigrations(ctx, app.configurator, fromVM)
+		m, err := app.ModuleManager.RunMigrations(ctx, app.configurator, fromVM)
+		if err != nil {
+			return m, err
+		}
+		sdkCtx := sdk.UnwrapSDKContext(ctx)
+		{
+			params := app.ICAHostKeeper.GetParams(sdkCtx)
+			msg := "/ibc.applications.interchain_accounts.host.v1.MsgModuleQuerySafe"
+			if !slices.ContainsFunc(params.AllowMessages, func(allowMsg string) bool {
+				return allowMsg == "*" || allowMsg == msg
+			}) {
+				params.AllowMessages = append(params.AllowMessages, msg)
+				app.ICAHostKeeper.SetParams(sdkCtx, params)
+			}
+		}
+		return m, nil
 	})
 
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
