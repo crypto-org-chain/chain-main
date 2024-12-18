8eb7fd1e824ef2b463a0a81633a23836fed51a50
HuangYi <huang@crypto.com>
2024-12-18 15:25:26 +0800

    bump version

--- default.nix
+++ default.nix
@@ -38,7 +38,7 @@ let
 in
 buildGoApplication rec {
   pname = "chain-maind";
-  version = "4.2.9";
+  version = "4.2.10";
   go = buildPackages.go_1_20;
   src = lib.cleanSourceWith {
     name = "src";
