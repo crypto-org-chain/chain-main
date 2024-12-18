efaf644db0a6f0dc3a31aaf312d6255eabbc59ab
mmsqe <mavis@crypto.com>
2024-12-18 16:43:05 +0800

    Problem: aarch64-darwin is not supported

--- scripts/release.sh
+++ scripts/release.sh
@@ -37,7 +37,11 @@ if [[ "$build_platform" == "x86_64-linux" ]]; then
 elif [[ "$build_platform" == "aarch64-linux" ]]; then
     hosts="Linux_arm64,native Linux_x86_64,gnu64 Windows_x86_64,mingwW64"
 elif [[ "$build_platform" == "x86_64-darwin" ]]; then
-    hosts="Darwin_x86_64,native Darwin_arm64,aarch64-darwin"
+    hosts="Darwin_x86_64,native"
+elif [[ "$build_platform" == "aarch64-darwin" ]]; then
+    # cross compiling to x86_64-darwin from aarch64-darwin is not supported
+    # see: https://github.com/NixOS/nixpkgs/issues/180771
+    hosts="Darwin_arm64,native"
 else
     echo "don't support build platform: $build_platform" 
     exit 1
