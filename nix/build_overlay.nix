e6bc271643068c1290f4628c2b2822ad916ce6b7
mmsqe <mavis@crypto.com>
2025-02-04 22:45:10 +0800

    bump go 1.23.4

--- nix/build_overlay.nix
+++ nix/build_overlay.nix
@@ -1,5 +1,12 @@
 # some basic overlays necessary for the build
 final: super: {
+  go_1_23 = super.go_1_23.overrideAttrs (old: rec {
+    version = "1.23.4";
+    src = final.fetchurl {
+      url = "https://go.dev/dl/go${version}.src.tar.gz";
+      hash = "sha256-rTRaxCHpCBQpOpaZzKGd1SOCUcP2h5gLvK4oSVsmNTE=";
+    };
+  });
   rocksdb = final.callPackage ./rocksdb.nix { };
   golangci-lint = final.callPackage ./golangci-lint.nix { };
 }
