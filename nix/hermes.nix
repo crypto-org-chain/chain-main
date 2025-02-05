b8486f0dfe61216b95e9b46cb09f7e5543d27dd5
mmsqe <mavis@crypto.com>
2025-02-05 10:46:24 +0800

    fix hermes

--- nix/hermes.nix
+++ nix/hermes.nix
@@ -6,16 +6,23 @@
   rustPlatform,
   symlinkJoin,
   openssl,
+  pkg-config,
 }:
+
 rustPlatform.buildRustPackage rec {
   name = "hermes";
   inherit src;
-  cargoSha256 = "sha256-DBHDJlD2kTUeU0LCG5ZUij+v+lWUYRPGVvUKUYPfYQA=";
   cargoBuildFlags = "-p ibc-relayer-cli";
   buildInputs = lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.Security
+    pkg-config
+    openssl
     darwin.libiconv
+    darwin.apple_sdk.frameworks.SystemConfiguration
   ];
+  cargoLock = {
+    lockFile = "${src}/Cargo.lock";
+  };
   doCheck = false;
   RUSTFLAGS = "--cfg ossl111 --cfg ossl110 --cfg ossl101";
   OPENSSL_NO_VENDOR = "1";
