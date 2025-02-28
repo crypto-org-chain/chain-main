6f96fe1c8e27368fc3a98597b8dfa15bac485fc9
mmsqe <mavis@crypto.com>
2025-02-28 10:52:29 +0800

    fix solo

--- nix/solomachine.nix
+++ nix/solomachine.nix
@@ -12,13 +12,13 @@ rustPlatform.buildRustPackage rec {
   version = "0.1.2";
 
   src = fetchFromGitHub {
-    owner = "yihuang";
+    owner = "mmsqe";
     repo = pname;
-    rev = "04a07839d4d1acadc805f728c505668d3225b70d";
-    sha256 = "sha256-jeh0ZO91cME3AaePujn0T7MG3Hsoy4qLWwIbYCspyac=";
+    rev = "95a08151c584565a975530c2a52d24082ccf4824";
+    sha256 = "sha256-qNifHvgI3dYUVGfbfkDCrTRJeB9+d8zC8gYJba7ATZg=";
   };
 
-  cargoSha256 = "sha256-bFusoI2vmQcjrELn1Kj252NHaWwbVymSuhE2B3M5QiU=";
+  cargoSha256 = "sha256-cuDc8h0Hb6fiizvhMKe3v2nPXkaIXx+1mgCH68KsB4g=";
   cargoBuildFlags = "-p solo-machine -p mnemonic-signer";
   nativeBuildInputs = [
     protobuf
