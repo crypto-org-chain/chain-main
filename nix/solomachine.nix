e14f6b85a44ca73b07b152cf7bf4d52877457cba
HuangYi <huang@crypto.com>
2025-03-11 09:59:17 +0800

    Problem: latest solomachine not tested

--- nix/solomachine.nix
+++ nix/solomachine.nix
@@ -12,13 +12,13 @@ rustPlatform.buildRustPackage rec {
   version = "main";
 
   src = fetchFromGitHub {
-    owner = "crypto-com";
+    owner = "devashishdxt";
     repo = pname;
-    rev = "18c80bf993c8715551793c0d881af16f1ed9958a";
-    sha256 = "sha256-f9WSerHGT2L6ervgNg2l9LPHwvNxTPvXydcODZ3P2p8=";
+    rev = "fae6e0cb3f49da9da460cc7200378e10d1fd63ce";
+    hash = "";
   };
 
-  cargoSha256 = "sha256-0A61jlQuJoci7SXh7fEW1dyKr4huDM9teh2ead2vFTI=";
+  cargoHash = "";
   cargoBuildFlags = "-p solo-machine -p mnemonic-signer";
   nativeBuildInputs = [
     protobuf
