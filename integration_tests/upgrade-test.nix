60f391ca42926b7162b0afe6e3df8fa8368b7d3b
Calvin Lau <calvin@crypto.com>
2025-05-19 20:52:44 +0800

    change upgrade plan to 6.0.0 as convention

--- integration_tests/upgrade-test.nix
+++ integration_tests/upgrade-test.nix
@@ -28,8 +28,8 @@ let
     (fetchFlake "crypto-org-chain/chain-main" "b3226f06fd2a236f9957304c4d83b0ea06ed2604").default;
   released4_3 =
     (fetchFlake "crypto-org-chain/chain-main" "7598bc46226a1b58116da2e6bd3903aca5b5313b").default;
-  released5_0_2 =
-    (fetchFlake "crypto-org-chain/chain-main" "f81a19a618a7f7c5981c63261ca3241d41ca0f4b").default;
+  released5_0 =
+    (fetchFlake "crypto-org-chain/chain-main" "246ba80c2f0c7e11a0a7b483a349d177ffeb0a9d").default;
   current = pkgs.callPackage ../. { };
 in
 pkgs.linkFarm "upgrade-test-package" [
@@ -54,11 +54,11 @@ pkgs.linkFarm "upgrade-test-package" [
     path = released4_3;
   }
   {
-    name = "v5.0";
-    path = released5_0_2;
+    name = "v5.0.0";
+    path = released5_0;
   }
   {
-    name = "v6.0";
+    name = "v6.0.0";
     path = current;
   }
 ]
