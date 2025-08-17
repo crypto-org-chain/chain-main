a338eb1bfc82d852691c6369da997b3ed10381ef
songgaoye <songgao.ye@crypto.com>
2025-08-17 15:49:06 +0800

    dump zero timestamp

--- cmd/chain-maind/app/versiondb.go
+++ cmd/chain-maind/app/versiondb.go
@@ -63,6 +63,7 @@ func VersionDBChangeSetGroupCmd(opts versiondbclient.Options) *cobra.Command {
 	cmd.AddCommand(
 		DumpVersionDBChangeSet(opts),
 		FixVersionDB(opts),
+		DumpZeroVersionDBChangeSet(opts),
 	)
 	return cmd
 }
@@ -143,6 +144,7 @@ func createFile(name string) (*os.File, error) {
 	return os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o600)
 }
 
+// / it's not right
 func FixVersionDB(opts versiondbclient.Options) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "fix-versiondb [versiondb-dir] [file-dir]",
@@ -228,6 +230,78 @@ func FixVersionDB(opts versiondbclient.Options) *cobra.Command {
 
 				versionDB.PutAtVersion(version, addSet)
 				versionDB.Flush()
+			}
+			return nil
+		},
+	}
+	cmd.Flags().Int64(flagVersion, 0, "the version to dump")
+	return cmd
+}
+
+func DumpZeroVersionDBChangeSet(opts versiondbclient.Options) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "dump-zeroversiondb [dir] [outDir]",
+		Short: "dump zeroversiondb changeset at version [dir] [outDir]",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			dir := args[0]
+			outDir := args[1]
+
+			version, err := cmd.Flags().GetInt64(flagVersion)
+			if err != nil {
+				return err
+			}
+			versionDB, err := tsrocksdb.NewStore(dir)
+			if err != nil {
+				return err
+			}
+			if err := os.MkdirAll(outDir, os.ModePerm); err != nil {
+				return err
+			}
+
+			for _, storeKey := range opts.DefaultStores {
+				it, err := versionDB.IteratorAtVersion(storeKey, nil, nil, &version)
+				if err != nil {
+					return err
+				}
+				defer it.Close()
+
+				kvsFile := filepath.Join(outDir, storeKey)
+				fpKvs, err := createFile(kvsFile)
+				if err != nil {
+					return err
+				}
+				kvsWriter := bufio.NewWriter(fpKvs)
+
+				var pairs []*iavl.KVPair
+				for ; it.Valid(); it.Next() {
+					if binary.LittleEndian.Uint64(it.Timestamp()) != 0 {
+						continue
+					}
+					key := make([]byte, len(it.Key()))
+					copy(key, it.Key())
+					value := make([]byte, len(it.Value()))
+					copy(value, it.Value())
+					pair := &iavl.KVPair{Key: key, Value: value}
+					if len(pair.Value) == 0 {
+						pair.Delete = true
+					}
+					pairs = append(pairs, pair)
+				}
+				changeset := &iavl.ChangeSet{Pairs: pairs}
+				// https://github.com/crypto-org-chain/cronos/blob/28bc916d0903050ac30ddd79805f451bc38923d3/versiondb/client/changeset.go#L43
+				err = versiondbclient.WriteChangeSet(kvsWriter, version, changeset)
+				if err != nil {
+					return err
+				}
+				err = kvsWriter.Flush()
+				if err != nil {
+					return err
+				}
+				err = fpKvs.Close()
+				if err != nil {
+					return err
+				}
 			}
 			return nil
 		},
