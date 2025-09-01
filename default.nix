cbf7589a2fffd011c8648d0ca8410d5574855243
Thomas N. <81727899+thomas-nguy@users.noreply.github.com>
2025-09-01 18:52:48 +0800

    release v6.0.2 (#1197)

--- default.nix
+++ default.nix
@@ -39,7 +39,7 @@ let
 in
 buildGoApplication rec {
   pname = "chain-maind";
-  version = "v6.0.1";
+  version = "v6.0.2";
   go = buildPackages.go_1_23;
   src = lib.cleanSourceWith {
     name = "src";
