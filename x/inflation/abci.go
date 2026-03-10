3642f533a2aa05dc364d7121fa69a3a6145d7a72
Randy Ang <randy.ang@cronoslabs.org>
2026-03-10 10:26:41 +0800

    remove comment

--- x/inflation/abci.go
+++ x/inflation/abci.go
@@ -12,7 +12,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/telemetry"
 )
 
-// BeginBlocker will check the total supply does not exceed the maximum supply and returns an error if it does.
 func BeginBlocker(ctx context.Context, k keeper.Keeper) error {
 	defer telemetry.ModuleMeasureSince(types.ModuleName, telemetry.Now(), telemetry.MetricKeyBeginBlocker)
 	params, err := k.GetParams(ctx)
