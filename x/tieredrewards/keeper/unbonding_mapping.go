e40175747834497435ff242a058ea7b93b0a2805
Randy Ang <randy.ang@cronoslabs.org>
2026-04-09 13:08:42 +0800

    move index and mapping to one file

--- x/tieredrewards/keeper/unbonding_mapping.go
+++ x/tieredrewards/keeper/unbonding_mapping.go
@@ -2,9 +2,53 @@ package keeper
 
 import (
 	"context"
+
+	"cosmossdk.io/collections"
+	"cosmossdk.io/collections/indexes"
+
+	"github.com/crypto-org-chain/chain-main/v8/x/tieredrewards/types"
 )
 
-// --- Unbonding (undelegation) mappings ---
+// --- Indexes ---
+type UnbondingMappingsIndexes struct {
+	ByPosition *indexes.Multi[uint64, uint64, uint64]
+}
+
+func (i UnbondingMappingsIndexes) IndexesList() []collections.Index[uint64, uint64] {
+	return []collections.Index[uint64, uint64]{i.ByPosition}
+}
+
+func newUnbondingMappingsIndexes(sb *collections.SchemaBuilder) UnbondingMappingsIndexes {
+	return UnbondingMappingsIndexes{
+		ByPosition: indexes.NewMulti(
+			sb,
+			types.UnbondingIdsByPositionKey,
+			"unbonding_ids_by_position",
+			collections.Uint64Key,
+			collections.Uint64Key,
+			func(_, positionID uint64) (uint64, error) {
+				return positionID, nil
+			},
+		),
+	}
+}
+
+func newRedelegationMappingsIndexes(sb *collections.SchemaBuilder) UnbondingMappingsIndexes {
+	return UnbondingMappingsIndexes{
+		ByPosition: indexes.NewMulti(
+			sb,
+			types.RedelegationIdsByPositionKey,
+			"redelegation_ids_by_position",
+			collections.Uint64Key,
+			collections.Uint64Key,
+			func(_, positionID uint64) (uint64, error) {
+				return positionID, nil
+			},
+		),
+	}
+}
+
+// --- Unbonding (undelegation) Mappings ---
 
 // setUnbondingPositionMapping stores unbondingId -> positionId and updates indexes.
 func (k Keeper) setUnbondingPositionMapping(ctx context.Context, unbondingId, positionId uint64) error {
